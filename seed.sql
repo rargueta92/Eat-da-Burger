dc61cf7e0200dfc0d708fa309fd49a17bcdb6c97
rargueta92 <rudy.argueta@aol.com>
2019-03-31 22:17:02 -0600

    burgerapplication

---
+++ seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Triple-Stacked Avocado Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cali-Chicken Cheeseburger', false);
